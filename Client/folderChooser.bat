6e41dad7416c8791654d82f52c98479c8c0e80b7
3RM1N3 <wangyu7439@gmail.com>
2021-07-19 20:36:09 +0800

    Delete folderChooser.bat

--- Client/folderChooser.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set "psCommand="(new-object -COM 'Shell.Application').BrowseForFolder(0,'Please choose a folder.',0,0).self.path""
-for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "folder=%%I"
-setlocal enabledelayedexpansion
-echo !folder!
