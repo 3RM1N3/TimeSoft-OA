77b6df662279311d08ae5c62168b5e066e05f0a3
3RM1N3 <wangyu7439@gmail.com>
2021-07-21 22:54:36 +0800

    update

--- test/main.go
+++ test/main.go
@@ -1,54 +1,23 @@
 package main
 
 import (
+	SP "TimeSoft-OA/SocketPacket"
 	"fmt"
 	"os"
 )
 
 func main() {
-	//fileSlice := make(chan []byte, 64)
+	fileSliceChan := make(chan SP.SocketPacket, 64)
 
-	f, _ := os.Open("a.zip")
-	stat, _ := f.Stat()
-	defer f.Close()
-	var partsNum int = 1/8388608 + 1
-	fmt.Println(partsNum, stat.Size())
-	/*
-		go func() {
-			f, _ := os.Open("a.zip")
-			defer f.Close()
+	go func() {
+		f, _ := os.Open("a.zip")
+		defer f.Close()
 
-			newPacket(f, fileSlice)
+		SP.NewZipPacket(f, fileSliceChan)
+	}()
 
-			stat, _ := f.Stat()
-			fmt.Println("总字节：", stat.Size())
-
-			if stat.Size() > 8388608 {
-				buf := make([]byte, 8388608)
-				for {
-					n, err := f.Read(buf)
-					if err != nil {
-						close(fileSlice)
-						return
-					} else {
-						fmt.Println("正常发送")
-						fileSlice <- buf[:n]
-					}
-				}
-			}
-		}()
-
-		for {
-			slice, ok := <-fileSlice
-			if !ok {
-				return
-			}
-			sp := SP.NewSocketPacket(slice)
-			fmt.Println(sp.String())
-		}
-	*/
-}
-
-func newPacket(f *os.File, ch chan []byte) {
-
+	for {
+		sp := <-fileSliceChan
+		fmt.Print(sp.String(), "\n\n")
+	}
 }
