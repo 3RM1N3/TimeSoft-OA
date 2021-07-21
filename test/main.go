cf28599008da891d22eba409539d57dca2057f20
3RM1N3 <wangyu7439@gmail.com>
2021-07-21 17:07:54 +0800

    update

--- test/main.go
+++ test/main.go
@@ -1,46 +1,54 @@
 package main
 
 import (
-	SP "TimeSoft-OA/SocketPacket"
 	"fmt"
-	"io"
 	"os"
 )
 
 func main() {
-	fileSlice := make(chan []byte, 64)
+	//fileSlice := make(chan []byte, 64)
 
-	go func() {
-		f, _ := os.Open("a.zip")
-		defer f.Close()
+	f, _ := os.Open("a.zip")
+	stat, _ := f.Stat()
+	defer f.Close()
+	var partsNum int = 1/8388608 + 1
+	fmt.Println(partsNum, stat.Size())
+	/*
+		go func() {
+			f, _ := os.Open("a.zip")
+			defer f.Close()
 
-		stat, _ := f.Stat()
-		fmt.Println("总字节：", stat.Size())
+			newPacket(f, fileSlice)
 
-		if stat.Size() > 8388608 {
-			buf := make([]byte, 8388608)
-			for {
-				n, err := f.Read(buf)
-				if err == io.EOF {
-					close(fileSlice)
-					return
-				} else if err != nil {
-					fmt.Println("错误")
-					return
-				} else {
-					fmt.Println("正常发送")
-					fileSlice <- buf[:n]
+			stat, _ := f.Stat()
+			fmt.Println("总字节：", stat.Size())
+
+			if stat.Size() > 8388608 {
+				buf := make([]byte, 8388608)
+				for {
+					n, err := f.Read(buf)
+					if err != nil {
+						close(fileSlice)
+						return
+					} else {
+						fmt.Println("正常发送")
+						fileSlice <- buf[:n]
+					}
 				}
 			}
-		}
-	}()
+		}()
 
-	for {
-		slice, ok := <-fileSlice
-		if !ok {
-			return
+		for {
+			slice, ok := <-fileSlice
+			if !ok {
+				return
+			}
+			sp := SP.NewSocketPacket(slice)
+			fmt.Println(sp.String())
 		}
-		sp := SP.NewSocketPacket(slice)
-		fmt.Println(sp.String())
-	}
+	*/
+}
+
+func newPacket(f *os.File, ch chan []byte) {
+
 }
