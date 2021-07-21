7b099eb9ed4d80051e8fd1dbcc7439bfd9344616
3RM1N3 <wangyu7439@gmail.com>
2021-07-21 17:04:57 +0800

    定义上传文件类型的结构体

--- SocketPacket/lib.go
+++ SocketPacket/lib.go
@@ -5,6 +5,30 @@ import (
 	"encoding/binary"
 )
 
+type PacketType byte
+type PacketEndByte byte
+
+type FileUploadJson struct {
+	FileName string
+	FileSize int64
+}
+
+const (
+	ZipArchive   PacketType = iota // 压缩文件档案
+	CommonReport                   // 普通汇报，用于汇报数据接受情况
+	FileUpload                     // 上传文件之前的汇报
+	FileRequest                    // 请求下载文件
+	Login                          // 用于登录
+	Notice                         // 用于通知
+	PullList                       // 客户端请求获得数据表
+	PushList                       // 服务端用于推送表
+)
+
+const (
+	NotOverEnd PacketEndByte = iota // 此包结束但文件未结束
+	OverEnd                         // 此包结束且文件已结束
+)
+
 func Uint16ToByte(i uint16) ([]byte, error) {
 	buf := new(bytes.Buffer)
 	err := binary.Write(buf, binary.BigEndian, i)
